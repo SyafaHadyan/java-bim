2673df54d0a976a2d6290b215e35e96bc159f3da
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2025-03-27 12:18:23 +0700

    feat: main menu pager

--- src/com/filkom/javabim/AppRun.java
+++ src/com/filkom/javabim/AppRun.java
@@ -1,21 +1,50 @@
 import java.util.Scanner;
 
+import util.Pager;
+
 public class AppRun {
+    static final String VERSION = "v1.0.0";
+
     public static void main(String[] args) {
-        Handler handler = new Handler();
         Scanner scanner = new Scanner(System.in);
+        Pager pager = new Pager();
+        Handler handler = new Handler();
 
         while (true) {
-            String userInputOption = scanner.nextLine();
-
+            pager.header("Java Book Inventory Manager " + VERSION);
+            pager.message("(0) Input Book");
+            pager.message("(1) Get All Books");
+            pager.message("(2) Search Book");
+            pager.message("(3) Update Book");
+            pager.message("(4) Delete Book");
+            pager.message("(5) Quit");
+            pager.spacer();
+            String userInputOption = pager.input();
             switch (userInputOption) {
-                case "1":
+                case "0":
+                    handler.create();
                     break;
-                default:
+                case "1":
+                    handler.get();
+                    break;
+                case "2":
+                    handler.search();
+                    break;
+                case "3":
+                    handler.update();
+                    break;
+                case "4":
+                    handler.delete();
+                    break;
+                case "5":
                     scanner.close();
+                    handler.quit();
                     System.exit(0);
+                default:
+                    handler.invalidInput();
+                    break;
             }
+            pager.footer();
         }
-
     }
 }
