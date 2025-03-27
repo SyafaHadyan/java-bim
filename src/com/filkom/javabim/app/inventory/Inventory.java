9e6165fa381ce268fbe4dc68b6e9b263c01878e4
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2025-03-27 12:29:22 +0700

    feat: add basic read write operation

--- src/com/filkom/javabim/app/inventory/Inventory.java
+++ src/com/filkom/javabim/app/inventory/Inventory.java
@@ -1,9 +1,31 @@
 package app.inventory;
 
-public class Inventory<T> {
-    private T t;
+import java.util.ArrayList;
+import java.util.UUID;
 
-    Inventory(T t) {
-        this.t = t;
+import app.book.Book;
+
+public class Inventory<T> {
+    private ArrayList<T> itemList = new ArrayList<>();
+
+    public void addItem(T item) {
+        itemList.add(item);
     }
+
+    public ArrayList<T> getAllItems() {
+        return this.itemList;
+    }
+
+    public ArrayList<T> searchItems(String keyword) {
+        return this.itemList;
+    }
+
+    public void updateItem(T item) {
+        itemList.add(item);
+    }
+
+    public void removeItem(T item) {
+        itemList.remove(item);
+    }
+
 }
